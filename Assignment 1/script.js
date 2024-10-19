a1d987238ad47a430ca31ca68b682c511b7d5bb1
Sam Peter <110963269+samthe030201@users.noreply.github.com>
2024-10-19 18:46:03 +0530

    Add files via upload

---
+++ Assignment 1/script.js
@@ -0,0 +1,13 @@
+
+function button_click() {
+    alert('Hello, welcome to my page! You have clicked the button')
+}
+
+
+ function mouse_on_picture() {
+    document.body.style.backgroundColor = '#B3E0DC';
+}
+
+function mouse_not_on_picture() {
+    document.body.style.backgroundColor = '#f0f0f0';
+}
